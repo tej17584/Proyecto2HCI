0bb3f7179d1841b094bbab3f9d7e3da62ee9ebb2
tej17584 <tej17584@uvg.edu.gt>
2019-05-25 23:39:29 -0600

    Envío de Email
    Envío de Email terminado

---
+++ Interfaz/metodos.js
@@ -0,0 +1,5 @@
+function submitForm() {
+  alert(
+    "Será redirigido a una página para verificar que sus datos fueron enviados. Si no ve una nueva página, contacte al Admin. Si la ve, retroceda."
+  );
+}
